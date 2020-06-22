289aa7d44ea034056365cfd6ded8d704ecbb1b0a
micmine <micmine4@gmail.com>
2020-06-22 22:08:30 +0200

    :sparkles: Add redirect on image upload

--- src/routes/ImageRouter.js
+++ src/routes/ImageRouter.js
@@ -13,214 +13,215 @@ var PublicObject = require("../controller/PublicObject");
 var Validation = require("../controller/Validation");
 
 module.exports = {
-  search: function (app) {
-    app.get("/image/search", (req, res) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, GET, POST, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      
-      const data = req.body;
-      console.log(data);
+	search: function (app) {
+		app.get("/image/search", (req, res) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header(
+				"Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS"
+			);
+			res.header("Access-Control-Allow-Headers", "Content-Type");
 
-      var validation = Validation.getByTitle(data);
+			const data = req.body;
+			console.log(data);
 
-      if (validation == true) {
-        Image.find({ title: data.title, status: 1 }, {}, (err, images) => {
-          res.status(200);
+			var validation = Validation.getByTitle(data);
 
-          var out = [];
+			if (validation == true) {
+				Image.find({ title: data.title, status: 1 }, {}, (err, images) => {
+					res.status(200);
 
-          var publicObject = new PublicObject();
+					var out = [];
 
-          images.forEach((image) => {
-            out.push(publicObject.image(image));
-          });
+					var publicObject = new PublicObject();
 
-          res.json(out);
-        });
-      } else {
-        res.status(409);
-        res.json(validation.errors);
-      }
-    });
-  },
-  list: function (app) {
-    app.get("/image/list", (req, res) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, GET, POST, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      
-      Image.find({ status: 1 }, {}, (err, images) => {
-        res.status(200);
+					images.forEach((image) => {
+						out.push(publicObject.image(image));
+					});
 
-        var out = [];
+					res.json(out);
+				});
+			} else {
+				res.status(409);
+				res.json(validation.errors);
+			}
+		});
+	},
+	list: function (app) {
+		app.get("/image/list", (req, res) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header(
+				"Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS"
+			);
+			res.header("Access-Control-Allow-Headers", "Content-Type");
 
-        var publicObject = new PublicObject();
+			Image.find({ status: 1 }, {}, (err, images) => {
+				res.status(200);
 
-        images.forEach((image) => {
-          out.push(publicObject.image(image));
-        });
+				var out = [];
 
-        res.json(out);
-      });
-    });
-  },
-  get: function (app) {
-    app.get("/image", (req, res) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, GET, POST, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
+				var publicObject = new PublicObject();
 
-      const data = req.body;
-      console.log(data);
+				images.forEach((image) => {
+					out.push(publicObject.image(image));
+				});
 
-      var validation = Validation.get(data);
+				res.json(out);
+			});
+		});
+	},
+	get: function (app) {
+		app.get("/image", (req, res) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header(
+				"Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS"
+			);
+			res.header("Access-Control-Allow-Headers", "Content-Type");
 
-      if (validation == true) {
-        Image.find({ uuid: data.uuid, status: 1 }, {}, (err, images) => {
-          res.status(200);
+			const data = req.body;
+			console.log(data);
 
-          var out = [];
+			var validation = Validation.get(data);
 
-          var publicObject = new PublicObject();
+			if (validation == true) {
+				Image.find({ uuid: data.uuid, status: 1 }, {}, (err, images) => {
+					res.status(200);
 
-          images.forEach((image) => {
-            out.push(publicObject.image(image));
-          });
+					var out = [];
 
-          res.json(out);
-        });
-      } else {
-        res.status(409);
-        res.json(validation.errors);
-      }
-    });
-  },
-  public: function (app) {
-    app.use("/public", express.static("public"));
-  },
+					var publicObject = new PublicObject();
 
-  upload: function (app) {
-    var upload = multer({ dest: process.env.uploadPath });
+					images.forEach((image) => {
+						out.push(publicObject.image(image));
+					});
 
-    app.post("/image/upload", upload.single("image"), (req, res) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, GET, POST, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
+					res.json(out);
+				});
+			} else {
+				res.status(409);
+				res.json(validation.errors);
+			}
+		});
+	},
+	public: function (app) {
+		app.use("/public", express.static("public"));
+	},
 
-      var current = uuid();
+	upload: function (app) {
+		var upload = multer({ dest: process.env.uploadPath });
 
-      const output = {
-        uuid: current,
-        title: req.body.title,
-        source: current + ".png",
-        status: 1,
-      };
+		app.post("/image/upload", upload.single("image"), (req, res) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header(
+				"Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS"
+			);
+			res.header("Access-Control-Allow-Headers", "Content-Type");
 
-      if (!req.file) {
-        const error = new Error("Please upload a file");
-        error.httpStatusCode = 400;
-        res.send(error);
-      }
+			var current = uuid();
 
-      var file = new LocalImage()
-        .save(output.uuid, req.file)
-        .then(() => {
-          const image = new Image(output);
+			const output = {
+				uuid: current,
+				title: req.body.title,
+				source: current + ".png",
+				status: 1,
+			};
 
-          image
-            .save()
-            .then(() => {
-              res.status(201);
-              res.json(output.uuid);
-              console.log("Create Image");
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(500);
-              res.send(err);
-            });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500);
-          res.json("Could not save Image");
-        });
-    });
-  },
-  post: function (app) {
-    app.post("/image", (req, res) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, GET, POST, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
+			if (!req.file) {
+				const error = new Error("Please upload a file");
+				error.httpStatusCode = 400;
+				res.send(error);
+			}
 
-      const data = req.body;
-      console.log(data);
-      var validation = Validation.upload(data);
+			var file = new LocalImage()
+				.save(output.uuid, req.file)
+				.then(() => {
+					const image = new Image(output);
 
-      if (validation == true) {
-        const output = {
-          uuid: uuid(),
-          title: data.title,
-          source: data.source,
-          status: 1,
-        };
+					image
+						.save()
+						.then(() => {
+							res.status(201);
+							res.json(output.uuid);
+							console.log("Create Image");
+						})
+						.catch((err) => {
+							console.log(err);
+							res.status(500);
+							res.send(err);
+						});
+				}).catch((err) => {
+					console.log(err);
+					res.status(500);
+					res.json("Could not save Image");
+				}).finally(() => {
+					res.redirect(req.param("redirect"));
+				});
+		});
+	},
+	post: function (app) {
+		app.post("/image", (req, res) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header(
+				"Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS"
+			);
+			res.header("Access-Control-Allow-Headers", "Content-Type");
 
-        const image = new Image(output);
+			const data = req.body;
+			console.log(data);
+			var validation = Validation.upload(data);
 
-        // save image
-        image
-          .save()
-          .then(() => {
-            res.status(201);
-            res.json(output.uuid);
-            console.log("Create Image:  " + data);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(500);
-            res.send(err);
-          });
-      } else {
-        res.status(409);
-        res.json(validation);
-      }
-    });
-  },
-  put: function (app) {
-    app.put("/image", (req, res) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, GET, POST, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      
-      const data = req.body;
-      console.log(data);
-      var validation = Validation.get(data);
+			if (validation == true) {
+				const output = {
+					uuid: uuid(),
+					title: data.title,
+					source: data.source,
+					status: 1,
+				};
 
-      if (validation == true) {
-        Image.updateOne({ uuid: data.uuid }, { status: data.status });
-      } else {
-        res.status(409);
-        res.json(validation.errors);
-      }
-    });
-  },
+				const image = new Image(output);
+
+				// save image
+				image
+					.save()
+					.then(() => {
+						res.status(201);
+						res.json(output.uuid);
+						console.log("Create Image:  " + data);
+					})
+					.catch((err) => {
+						console.log(err);
+						res.status(500);
+						res.send(err);
+					});
+			} else {
+				res.status(409);
+				res.json(validation);
+			}
+		});
+	},
+	put: function (app) {
+		app.put("/image", (req, res) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header(
+				"Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS"
+			);
+			res.header("Access-Control-Allow-Headers", "Content-Type");
+
+			const data = req.body;
+			console.log(data);
+			var validation = Validation.get(data);
+
+			if (validation == true) {
+				Image.updateOne({ uuid: data.uuid }, { status: data.status });
+			} else {
+				res.status(409);
+				res.json(validation.errors);
+			}
+		});
+	},
 };
